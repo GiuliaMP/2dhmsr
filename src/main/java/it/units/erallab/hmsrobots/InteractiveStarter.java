b52d005106df871e6bc8951da934dcf0bd584db4
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2022-04-05 16:54:26 +0200

    Small changes.

--- src/main/java/it/units/erallab/hmsrobots/InteractiveStarter.java
+++ src/main/java/it/units/erallab/hmsrobots/InteractiveStarter.java
@@ -1,31 +1,58 @@
 package it.units.erallab.hmsrobots;
 
-import it.units.erallab.hmsrobots.core.controllers.*;
+import it.units.erallab.hmsrobots.core.controllers.SmoothedController;
+import it.units.erallab.hmsrobots.core.controllers.TimeFunctions;
 import it.units.erallab.hmsrobots.core.interactive.BasicInteractiveController;
 import it.units.erallab.hmsrobots.core.interactive.DevicePoller;
 import it.units.erallab.hmsrobots.core.interactive.InteractiveSnapshotListener;
 import it.units.erallab.hmsrobots.core.interactive.KeyboardPoller;
 import it.units.erallab.hmsrobots.core.objects.Robot;
+import it.units.erallab.hmsrobots.core.objects.Voxel;
 import it.units.erallab.hmsrobots.tasks.locomotion.Locomotion;
 import it.units.erallab.hmsrobots.tasks.locomotion.Outcome;
 import it.units.erallab.hmsrobots.util.Grid;
 import it.units.erallab.hmsrobots.util.RobotUtils;
+import it.units.erallab.hmsrobots.util.SerializationUtils;
+import it.units.erallab.hmsrobots.viewers.GridOnlineViewer;
+import it.units.erallab.hmsrobots.viewers.NamedValue;
 import it.units.erallab.hmsrobots.viewers.drawers.Drawers;
 import org.dyn4j.dynamics.Settings;
 
-import java.io.File;
-import java.util.List;
-import java.util.SortedMap;
+import java.util.Random;
 
 public class InteractiveStarter {
 
     public static void main(String[] args) {
-        multiped();
+        //multiped();
+        plainWorm();
     }
 
     private static void multiped() {
         double f = 1d;
         Grid<Boolean> body = RobotUtils.buildShape("biped-8x4");
+        BasicInteractiveController basicInteractiveController = new BasicInteractiveController();
+        Robot robot = new Robot(
+                new SmoothedController(basicInteractiveController, 3),
+                RobotUtils.buildSensorizingFunction("uniform-a-0.01").apply(body)
+        );
+        Locomotion locomotion = new Locomotion(5,
+                Locomotion.createTerrain("hilly-0.5-10-0"),
+                new Settings()
+        );
+
+        //DevicePoller devicePoller = new JoystickSnapshotListener(basicInteractiveController);
+        DevicePoller devicePoller = new KeyboardPoller(basicInteractiveController);
+        InteractiveSnapshotListener interactiveSnapshotListener = new InteractiveSnapshotListener(1d / 60d, Drawers.basic(), devicePoller, basicInteractiveController);
+        Outcome out = locomotion.apply(robot, interactiveSnapshotListener);
+        /*SortedMap<Double, Outcome.Observation> observationsHistory = out.getObservations();
+        SortedMap<Double, List<Boolean>> flagsHistory = interactiveSnapshotListener.getFlagHistory();
+        //File file = new File("DatiUtili.csv");
+        //WriteToFile.toFile(file, observationsHistory, flagsHistory);*/
+    }
+
+    private static void plainWorm() {
+        double f = 1d;
+        Grid<Boolean> body = RobotUtils.buildShape("worm-8x2");
         BasicInteractiveController basicInteractiveController = new BasicInteractiveController();
         Robot robot = new Robot(
                 new SmoothedController(basicInteractiveController, 3),
